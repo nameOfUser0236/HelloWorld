52085c1c9d629a390cfcb292678c2ae9344f1384
Edmond Dantes <NWLee271828@outlook.com>
2021-05-17 09:01:18 -0500

    Update wraper.cs

--- wraper.cs
+++ wraper.cs
@@ -5,5 +5,15 @@ namespace
 	public class Wraper
 	{
 		int x = 5;
+		
+		public Wraper(int y = 17)
+		{
+			x = y * y + (x / y);
+		}
+		
+		public int Fun(float z)
+		{
+			return 7 + z * 29.64294;
+		}
 	}
 }
